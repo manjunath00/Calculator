b72d17f957460856f42e61b567d28b7e79f5513b
Manjunath <kadrimanjunath56@gmail.com>
2019-03-26 23:00:15 +0530

    This is the first working prototype of calculator.

--- script.js
+++ script.js
@@ -1,74 +1,110 @@
-var first, second, third, total, firstValue, secondValue, thirdValue;
-first ='';
-second = '';
-third = '';
-total = 0;
-firstValue = true;
-thirdValue = false;
-var calculate = function(second) {
 
-    switch (second) {
 
-        case 'minus':
-            total = parseFloat(first) - parseFloat(third);
+// 1. Display the number clicked
+
+// 2. Switch on the operations
+let result, last, firstStatus, lastStatus, symbol;
+
+first = '';
+
+last = '';
+
+firstStatus = true;
+
+lastStatus = false;
+
+
+
+let firstNum = function(e) {
+    first += e.target.textContent;
+    console.log(first);
+    document.querySelector('#display').textContent = first;
+    return first;
+};
+
+let secondNum = function(e) {
+    last += e.target.textContent;
+    console.log(last);
+
+    document.querySelector('#display').textContent = last;
+    return last;
+};
+
+let operation = function(e) {
+    symbol = e.target.textContent;
+    document.querySelector('#display').textContent = symbol;
+    firstStatus = false;
+    lastStatus = true;
+    console.log(e.target.textContent);
+    // switcher(e);
+};
+
+let math_operation = function(symbol) {
+    // console.log(symbol);
+
+    switch(symbol) {
+        case '+':
+            result = parseInt(first) + parseInt(last);
             break;
 
-        case 'plus':
-            total = parseFloat(first) + parseFloat(third);
-            break;
-     
-        case 'multiply':
-            total = parseFloat(first) * parseFloat(third);
+        case '-':
+            result = parseInt(first) - parseInt(last);
             break;
 
-        case 'div':
-            total = parseFloat(first) / parseFloat(third);
+        case '/':
+            result = parseFloat(first) / parseFloat(last);
             break;
-    };
 
-    console.log(total);
+        case 'x':
+            result = parseInt(first) * parseInt(last);
+            break;   
+    }
+    // console.log(result);
+    return result;
+};
+
+let switcher = function(e) {
+
+    if (firstStatus) {
+        firstNum(e);
+    } else {
+        secondNum(e);
+    }
 
 };
+
+
+let displayResult = function() {
+    math_operation(symbol);
+    console.log(first);
+    console.log(symbol);
+    console.log(last);
+    console.log(result);
+    document.querySelector('#display').textContent = '='+result;
+
+}
+
+
+document.querySelector('#one').addEventListener('click', switcher);
+document.querySelector('#two').addEventListener('click', switcher);
+document.querySelector('#three').addEventListener('click', switcher);
+document.querySelector('#four').addEventListener('click', switcher);
+document.querySelector('#five').addEventListener('click', switcher);
+document.querySelector('#six').addEventListener('click', switcher);
+document.querySelector('#seven').addEventListener('click', switcher);
+document.querySelector('#eight').addEventListener('click', switcher);
+document.querySelector('#nine').addEventListener('click', switcher);
+document.querySelector('#zero').addEventListener('click', switcher);
+
+document.querySelector('#plus').addEventListener('click', operation);
+document.querySelector('#minus').addEventListener('click', operation);
+document.querySelector('#multiply').addEventListener('click', operation);
+document.querySelector('#div').addEventListener('click', operation);
+
+document.querySelector('#equals').addEventListener('click', displayResult);
+
 /*
 
-document.getElementById('reset').addEventListener('click', function() {
+When '/' is clicked
 
-    console.log('Calculator is reset');
-});
 */
-
-document.querySelector('.numbers').addEventListener('click', function(e) {
-    
-    var val = e.target.id;
-    if (firstValue) {
-        first += val;
-        console.log(first);
-        thirdValue = false;
-    } else if (thirdValue) {
-        third += val;
-        console.log(third);
-    }
-
-});
-
-
-document.querySelector('.operation').addEventListener('click', function(e) {
-
-
-    if (firstValue) {
-        firstValue = false;
-        var second = e.target.id;
-        console.log(second);
-        thirdValue = true;
-    }
-
-});
-
-document.querySelector('.equal').addEventListener('click', function(e) {
-        calculate(second); 
-//    console.log(parseFloat(first) - parseFloat(third));
-});
-
-
-
-
